47b4146b55d7493665842f81b984756f32f3693e
Monurh <74192667+Monurh@users.noreply.github.com>
2023-03-20 10:35:24 +0200

    Add files via upload

---
+++ lab 10/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private IFile _fileProxy;
+        public Form1()
+        {
+            InitializeComponent();
+            _fileProxy = new FileProxy(@"C:\example.txt");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _fileProxy.Read();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _fileProxy.Write(textBox1.Text);
+            MessageBox.Show("Запис додано до файлу.", "Успіх");
+        }
+    }
+}
---
+++ lab 10/WindowsFormsApp1/WindowsFormsApp1/Proxy.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+
+    interface IFile
+    {
+        void Read();
+        void Write(string text);
+    }
+
+    class File : IFile
+    {
+        private readonly string _filePath;
+
+        public File(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public void Read()
+        {
+            using (var reader = new StreamReader(_filePath))
+            {
+                MessageBox.Show(reader.ReadToEnd(), "Вміст файлу");
+            }
+        }
+
+        public void Write(string text)
+        {
+            using (var writer = new StreamWriter(_filePath, true))
+            {
+                writer.WriteLine(text);
+            }
+        }
+    }
+
+    class FileProxy : IFile
+    {
+        private readonly string _filePath;
+        private File _file;
+
+        public FileProxy(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public void Read()
+        {
+            if (_file == null)
+            {
+                _file = new File(_filePath);
+            }
+
+            _file.Read();
+        }
+
+        public void Write(string text)
+        {
+            if (_file == null)
+            {
+                _file = new File(_filePath);
+            }
+
+            _file.Write(text);
+        }
+    }
+}
